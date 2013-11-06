7bea3adaf8d55529d7620009a4248436774af9ee
Vytautas A <vytautas.av@gmail.com>
2013-11-06 16:44:14 +0200

    File for inventory.

---
+++ MayhemAndHell/src/com/friendlyblob/mayhemandhell/client/entities/gui/SlotObject.java
@@ -0,0 +1,25 @@
+package com.friendlyblob.mayhemandhell.client.entities.gui;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.friendlyblob.mayhemandhell.client.helpers.Assets;
+
+public class SlotObject extends Rectangle {
+	
+	public SlotObject(int size) {
+		super(0, 0, size, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int iconId;
+	
+	public TextureRegion texture = Assets.getTextureRegion("icons/sword");
+	
+	@Override
+	public Rectangle setPosition(float x, float y) {
+		this.x = x - width/2;
+		this.y = y - height/2;
+		
+		return this;
+	}
+}
