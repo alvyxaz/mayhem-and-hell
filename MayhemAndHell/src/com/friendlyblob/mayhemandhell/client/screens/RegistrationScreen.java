456475d9d4f8ef85cdfeb5bbf72d35e04bcdf2ce
Vytautas A <vytautas.av@gmail.com>
2014-01-10 16:47:32 +0200

    Removed ugly cursor, functioning back button from reg.

--- MayhemAndHell/src/com/friendlyblob/mayhemandhell/client/screens/RegistrationScreen.java
+++ MayhemAndHell/src/com/friendlyblob/mayhemandhell/client/screens/RegistrationScreen.java
@@ -142,7 +142,7 @@ public class RegistrationScreen extends BaseScreen{
         backButton.addListener(new ChangeListener() {
     		@Override
             public void changed (ChangeEvent event, Actor actor) {
-    			System.out.println("wat do?");
+    			game.setScreen(game.screenLogin);
             }
         });
         
